f1ff1785e9072018f9821d8b810ebfc77f89d612
Cam K Weston <cameron.weston@uvm.edu>
2019-02-22 15:33:06 -0500

    Took out print statement for input runner start

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -17,8 +17,6 @@ public class Runner {
 
 	public static void main(String[] args) {
 		SQLiteDatabase db = new SQLiteDatabase();
-		
-		System.out.println("Starting user input runner");
 		InputRunner ir = new InputRunner(db);
 	}
 
